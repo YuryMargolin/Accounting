4660240f75409d74e8c6252db291515df8320c5e
margolin <yurymargolin@inbox.ru>
2025-08-27 20:11:45 +0300

    extraReducers added

--- src/features/token/tokenSlice.ts
+++ src/features/token/tokenSlice.ts
@@ -1,4 +1,5 @@
 import {createSlice, type PayloadAction} from "@reduxjs/toolkit";
+import {changePassword, fetchUser, registerUser} from "../api/accountApi.ts";
 
 const initialState = '';
 
@@ -8,6 +9,12 @@ const tokenSlice = createSlice({
     reducers: {
         setToken: (_state, action: PayloadAction<string>) => action.payload,
         clearToken: () => initialState,
+    },
+    extraReducers: builder => {
+        builder
+            .addCase(registerUser.fulfilled, (_state, action) => action.payload.token)
+            .addCase(fetchUser.fulfilled, (_state, action) => action.payload.token)
+            .addCase(changePassword.fulfilled, (_state, action) => action.payload);
     }
 })
 
