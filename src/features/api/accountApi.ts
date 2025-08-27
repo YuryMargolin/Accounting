6e78ba8ce3d6ed86ede738d780d8c75c3a4e8d3f
margolin <yurymargolin@inbox.ru>
2025-08-27 19:37:16 +0300

    Updating updateUser/changePassword

--- src/features/api/accountApi.ts
+++ src/features/api/accountApi.ts
@@ -1,6 +1,7 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
-import type {UserRegister} from "../../utils/types";
+import type {UserData, UserRegister} from "../../utils/types";
 import {base_url, createToken} from "../../utils/constants.ts";
+import type {RootState} from "../../app/store.ts";
 
 export const registerUser = createAsyncThunk(
     'user/register',
@@ -43,3 +44,48 @@ export const fetchUser = createAsyncThunk(
         return {user: data, token};
     }
 )
+
+export const updateUser =
+    createAsyncThunk<UserData, UserData, { state: RootState }>(
+        'user/update',
+        async (user, {getState}) => {
+            const response = await fetch(`${base_url}/account/user/${getState().user.login}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-type': 'application/json',
+                    Authorization: getState().token
+                },
+                body: JSON.stringify(user)
+            })
+            if (response.status === 401) {
+                throw new Error(`login or password is incorrect`);
+            }
+            if (!response.ok) {
+                throw new Error(`Something went wrong`);
+            }
+            const {firstName, lastName} = await response.json();
+            return {firstName, lastName};
+        }
+    )
+
+export const changePassword =
+    createAsyncThunk<string, string, { state: RootState }>
+    (
+        'user/password',
+        async (newPassword: string, {getState}) => {
+            const response = await fetch(`${base_url}/account/password`, {
+                method: 'PATCH',
+                headers: {
+                    Authorization: getState().token,
+                    'X-Password': newPassword
+                }
+            })
+            if (response.status === 401) {
+                throw new Error(`login or password is incorrect`);
+            }
+            if (!response.ok) {
+                throw new Error(`Something went wrong`);
+            }
+            return createToken(getState().user.login, newPassword);
+        }
+    )
