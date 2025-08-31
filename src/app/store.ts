4f4bff2d3ae29d7cf28a084722c2216f4c5d0ef5
margolin <yurymargolin@inbox.ru>
2025-08-31 18:45:00 +0300

    Sync state with local storage

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,16 @@
 import {configureStore} from "@reduxjs/toolkit";
 import token from "../features/token/tokenSlice";
 import user from "../features/user/userSlice";
+import type {UserProfile} from "../utils/types";
 
 export const store = configureStore({
     reducer: {
         token, user
     },
+    preloadedState: JSON.parse(localStorage.getItem('state') || '{}') as {token: string, user: UserProfile}
 })
+
+store.subscribe(() => localStorage.setItem("state", JSON.stringify(store.getState())));
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
